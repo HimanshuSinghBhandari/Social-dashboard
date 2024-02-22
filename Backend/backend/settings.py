be2d96b223a40f67f76267066c8276c27bf294a3
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-02-22 14:38:27 +0530

    trying to remove 404 error

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY='django-insecure--o+^2-knvxng9b0p=hz8g6-l-y8h15%vn7)v2luv_2#$lq_xef'
 DEBUG=True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', "*"]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
@@ -144,3 +144,6 @@ STATIC_ROOT = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
